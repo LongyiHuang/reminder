d18a69d11ee8a0e08b3a5a7e5443ef3e9d151f51
huanglongyi <475865662@qq.com>
2018-11-12 10:45:38 +0800

    Updates

---
+++ precache-manifest.1cb3874bbf9c44557a67e8354b0d3829.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0c1f8d1e35cfc77d4c2",
+    "url": "/reminder/static/js/runtime~main.a0c1f8d1.js"
+  },
+  {
+    "revision": "411b89ae7a419f07957d",
+    "url": "/reminder/static/js/main.411b89ae.chunk.js"
+  },
+  {
+    "revision": "e6a82a21c981efe56567",
+    "url": "/reminder/static/js/1.e6a82a21.chunk.js"
+  },
+  {
+    "revision": "411b89ae7a419f07957d",
+    "url": "/reminder/static/css/main.36f13b15.chunk.css"
+  },
+  {
+    "revision": "27948bde99a6ff745eda2415b436a73b",
+    "url": "/reminder/index.html"
+  }
+];
